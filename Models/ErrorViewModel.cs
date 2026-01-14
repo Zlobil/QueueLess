0a3f6a3bc0ba30d2d755fbbeb84bc07f5674f07e
Zlobil <stoycho.yakimov@gmail.com>
2026-01-14 20:44:39 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QueueLess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
