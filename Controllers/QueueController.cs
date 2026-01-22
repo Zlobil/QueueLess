f18dca1d846a5ea02e551602e47ac0de9ce58a9a
Zlobil <stoycho.yakimov@gmail.com>
2026-01-22 12:39:49 +0200

    Add ViewModel and Controller logic for Active queues page.

--- Controllers/QueueController.cs
+++ Controllers/QueueController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QueueLess.Data;
+using QueueLess.ViewModels;
 
 namespace QueueLess.Controllers
 {
     public class QueueController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public QueueController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -35,7 +44,20 @@ namespace QueueLess.Controllers
 
         public IActionResult Active()
         {
-            return View();
+            var queues = context.Queues
+                .Where(q => q.IsOpen)
+                .OrderBy(q => q.CreatedOn)
+                .Select(q => new QueueActiveViewModel
+                {
+                    Id = q.Id,
+                    Name = q.Name,
+                    Description = q.Description,
+                    AverageServiceTimeMinutes = q.AverageServiceTimeMinutes,
+                    IsOpen = q.IsOpen
+                })
+                .ToList();
+
+            return View(queues);
         }
 
         public IActionResult Public(int id = 1)
